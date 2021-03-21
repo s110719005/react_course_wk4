ee9942a9216eb3586cffb4227a02e08a1f491143
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-03-22 02:06:39 +0800

    finish choose color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home'
+import Product from './pages/Product'
+import Tableware from './pages/Tableware'
+import Cookware from './pages/Cookware'
+import HomeAccessories from './pages/HomeAccessories'
+import Lighting from './pages/Lighting'
+import Textile from './pages/Textile'
+import Furniture from './pages/Furniture'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/tableware" component={Tableware} />
+        <Route path="/cookware" component={Cookware} />
+        <Route path="/product/:productId" component={Product} />
+        <Route path="/home-accessories" component={HomeAccessories} />
+        <Route path="/lighting" component={Lighting} />
+        <Route path="/textile" component={Textile} />
+        <Route path="/furniture" component={Furniture} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
